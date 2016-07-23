2ed60bdcae90674429a7f67dd1cca3531b46afbe
Danny Bravman <dbravman@gmail.com>
2016-07-22 22:34:18 -0500

    queue

--- queue.rb
+++ queue.rb
@@ -1,3 +1,22 @@
-class MyQueue
+require_relative "linked_list"
 
+class MyQueue < LinkedList
+  def enqueue(element)
+    insert_last(element)
+  end
+
+  def dequeue
+    element = peel
+    remove_first
+    element
+  end
+
+  def peel
+    return nil if empty?
+    get(0)
+  end
+
+  def empty?
+    size == 0
+  end
 end
